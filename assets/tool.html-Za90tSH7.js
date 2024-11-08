f8fc12b7789dc1219bcf5433da947d40d782c940
xuuub66 <504077806@qq.com>
2024-11-08 13:30:01 +0800

    deploy

---
+++ assets/tool.html-Za90tSH7.js
@@ -0,0 +1,24 @@
+import{_ as l}from"./plugin-vue_export-helper-DlAUqK2U.js";import{r as a,c,b as t,a as e,e as i,d as s,o as r}from"./app-DVK9OJGa.js";const d={},p=t("h1",{id:"在线工具",tabindex:"-1"},[t("a",{class:"header-anchor",href:"#在线工具"},[t("span",null,"在线工具")])],-1),h=t("div",{class:"hint-container tip"},[t("p",{class:"hint-container-title"},"好用的在线工具")],-1),m={id:"ai",tabindex:"-1"},g={class:"header-anchor",href:"#ai"},_={id:"图标字体",tabindex:"-1"},f={class:"header-anchor",href:"#图标字体"},u={id:"code",tabindex:"-1"},y={class:"header-anchor",href:"#code"},k=t("hr",null,null,-1);function v(x,b){const o=a("MyIcon"),n=a("VPCard");return r(),c("div",null,[p,h,t("h2",m,[t("a",g,[t("span",null,[e(o,{name:"AI"}),i(" AI")])])]),e(n,{title:"AI问答网站，kimi。",desc:"很好用，AI问答网站，kimi。",logo:"https://cdn.jsdelivr.net/gh/20000428/PictureBed//img/kimi.svg",link:"https://kimi.moonshot.cn/"}),t("h2",_,[t("a",f,[t("span",null,[e(o,{name:"qianduan"}),i(" 图标字体")])])]),e(n,{title:"字体。",desc:"字体。",logo:"https://cdn.jsdelivr.net/gh/20000428/PictureBed//img/zhaoziwang.svg",link:"https://www.zhaozi.cn/"}),e(n,{title:"阿里巴巴矢量图标库。",desc:"阿里巴巴矢量图标库。",logo:"https://cdn.jsdelivr.net/gh/20000428/PictureBed//img/iconfont.svg",link:"https://www.iconfont.cn/?spm=a313x.search_index.i3.2.647e3a81OtpFsQ"}),e(n,{title:"翻译",desc:"在线翻译",logo:"https://static.deepl.com/img/favicon/tile_144.png",link:"https://www.deepl.com/zh/translator"}),t("h2",u,[t("a",y,[t("span",null,[e(o,{name:"code"}),i(" code")])])]),e(n,{title:"在线XML文件",desc:"在线XML文件处理",logo:"https://cdn.jsdelivr.net/gh/20000428/PictureBed//img/xml.webp",link:"https://tool.p2hp.com/tool-format-xml/"}),e(n,{title:"json处理",desc:"Json在线解析",logo:"https://s1.aigei.com/src/img/png/3d/3d34578d9e8e4dd3ac4d3c92292eb5a1.png?imageMogr2/auto-orient/thumbnail/!282x282r/gravity/Center/crop/282x282/quality/85/%7CimageView2/2/w/282&e=1735488000&token=P7S2Xpzfz11vAkASLTkfHN7Fw-oOZBecqeJaxypL:BvpQNiylKCkgGx6dELxCM0LpqLA=",link:"https://tools8.cn/json.html"}),s(` ## <MyIcon name="xiangce" /> 图片 
+
+<VPCard
+  title="超好用的图片压缩工具"
+  desc="超好用的图片压缩工具"
+  logo="https://tinypng.com/images/apng/panda-waving.png"
+  link="https://tinypng.com"
+/>
+
+<VPCard
+  title="golang 实现获取 每日一图，每 8 小时自动更新一次 , 跟 bing 每日一图保持一致"
+  desc="golang 实现获取 每日一图，每 8 小时自动更新一次 , 跟 bing 每日一图保持一致"
+  logo="http://file.mo7.cc/static/logo/512.png"
+  link="//file.mo7.cc/daily_cover"
+/>
+
+## <MyIcon name="jinrong" /> 金融
+
+<VPCard
+  title="博主本人编写的，基于大数据和深度学习 , 打造的量化交易工具。"
+  desc="博主本人编写的，基于大数据和深度学习 , 打造的量化交易工具。"
+  logo="https://file.mo7.cc/AItrade/logo/app.svg"
+  link="https://trade.mo7.cc"
+/> `),k])}const C=l(d,[["render",v],["__file","tool.html.vue"]]),A=JSON.parse('{"path":"/favorite/links/tool.html","title":"工具箱","lang":"zh-CN","frontmatter":{"icon":"gongju","order":2,"star":4,"title":"工具箱","date":"2024-08-01T00:00:00.000Z","pageInfo":["Author","PageView"],"description":"在线工具 好用的在线工具","head":[["meta",{"property":"og:url","content":"https://blog.ysf.cc/favorite/links/tool.html"}],["meta",{"property":"og:site_name","content":"LeafinWind"}],["meta",{"property":"og:title","content":"工具箱"}],["meta",{"property":"og:description","content":"在线工具 好用的在线工具"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2024-09-14T00:57:29.000Z"}],["meta",{"property":"article:author","content":"叶随风"}],["meta",{"property":"article:published_time","content":"2024-08-01T00:00:00.000Z"}],["meta",{"property":"article:modified_time","content":"2024-09-14T00:57:29.000Z"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"工具箱\\",\\"image\\":[\\"\\"],\\"datePublished\\":\\"2024-08-01T00:00:00.000Z\\",\\"dateModified\\":\\"2024-09-14T00:57:29.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"叶随风\\",\\"email\\":\\"504077806@qq.com\\"}]}"],["link",{"rel":"alternate","type":"application/atom+xml","href":"https://blog.ysf.cc/atom.xml","title":"LeafinWind Atom Feed"}],["link",{"rel":"alternate","type":"application/json","href":"https://blog.ysf.cc/feed.json","title":"LeafinWind JSON Feed"}],["link",{"rel":"alternate","type":"application/rss+xml","href":"https://blog.ysf.cc/rss.xml","title":"LeafinWind RSS Feed"}]]},"headers":[{"level":2,"title":"AI","slug":"ai","link":"#ai","children":[]},{"level":2,"title":"图标字体","slug":"图标字体","link":"#图标字体","children":[]},{"level":2,"title":"code","slug":"code","link":"#code","children":[]}],"git":{"createdTime":1722500593000,"updatedTime":1726275449000,"contributors":[{"name":"xuuub66","email":"504077806@qq.com","commits":7}]},"readingTime":{"minutes":1.2,"words":360},"filePathRelative":"favorite/links/tool.md","localizedDate":"2024年8月1日","excerpt":"\\n<div class=\\"hint-container tip\\">\\n<p class=\\"hint-container-title\\">好用的在线工具</p>\\n</div>\\n<h2></h2>\\n","autoDesc":true}');export{C as comp,A as data};
