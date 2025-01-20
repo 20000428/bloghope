d123e5901848563c43aeae28ab537a92d5a9b563
xuuub66 <504077806@qq.com>
2025-01-20 17:04:19 +0800

    Init

--- src/.vuepress/components/PreviewImage.vue
+++ src/.vuepress/components/PreviewImage.vue
@@ -1,240 +1,3 @@
-<script setup lang="ts">
-import { onMounted, nextTick, ref } from 'vue';
-import { useRouter } from 'vue-router';
-
-import MyIcon from './MyIcon.vue';
-
-let ImageArr = ref([]);
-let ShowPreviewBox = ref(false);
-let CurrentImgIdx = ref(0);
-
-function ClosePreviewBox() {
-  ShowPreviewBox.value = false;
-  document.body.style.overflow = 'auto';
-}
-
-function zoomFunc() {
-  const imgElm = document.getElementById('Mo7PreviewBox-img');
-
-  let width = imgElm.clientWidth;
-  let height = imgElm.clientHeight;
-  width = width * 1.1;
-  imgElm.style.width = width + 'px';
-  height = height * 1.1;
-  imgElm.style.height = height + 'px';
-}
-
-function shrinkFunc() {
-  const imgElm = document.getElementById('Mo7PreviewBox-img');
-  let width = imgElm.clientWidth;
-  let height = imgElm.clientHeight;
-  width = width * 0.9;
-  imgElm.style.width = width + 'px';
-  height = height * 0.9;
-  imgElm.style.height = height + 'px';
-}
-
-function fullscreenFunc() {
-  const imgElm = document.getElementById('Mo7PreviewBox-img');
-  window.open(imgElm.getAttribute('src'));
-}
-
-function leftFunc() {
-  if (CurrentImgIdx.value > 0) {
-    CurrentImgIdx.value--;
-  }
-  nextTick(() => {
-    ImageBoxReset();
-  });
-}
-
-function rightFunc() {
-  if (CurrentImgIdx.value < ImageArr.value.length - 1) {
-    CurrentImgIdx.value++;
-  }
-  nextTick(() => {
-    ImageBoxReset();
-  });
-}
-
-function ImageBoxReset() {
-  const boxElm = document.getElementById('Mo7PreviewBox');
-  const boxWidth = boxElm.clientWidth;
-  const boxHeight = boxElm.clientHeight;
-
-  const imgElm = document.getElementById('Mo7PreviewBox-img');
-
-  imgElm.style.width = boxWidth * 0.9 + 'px';
-  imgElm.style.height = '';
-  imgElm.style.maxWidth = '';
-  imgElm.style.maxHeight = '';
-  imgElm.style.left = '';
-  imgElm.style.top = '';
-  imgElm.style.cursor = '';
-
-  const height = imgElm.clientHeight;
-
-  if (height > boxHeight) {
-    imgElm.style.width = '';
-    imgElm.style.height = boxHeight * 0.9 + 'px';
-  }
-}
-
-function ImageClick(e) {
-  const elm = e.target;
-  const src = elm.getAttribute('src');
-  let index = CurrentImgIdx.value;
-  for (let i = 0; i < ImageArr.value.length; i++) {
-    const el = ImageArr.value[i];
-    if (el.src === src) {
-      index = i;
-      break;
-    }
-  }
-  CurrentImgIdx.value = index;
-  ShowPreviewBox.value = true;
-  document.body.style.overflow = 'hidden';
-
-  nextTick(() => {
-    ImageBoxReset();
-  });
-}
-
-function InitPreviewImage(type) {
-  console.log('初始化图片预览', type);
-  let contentElms = document.getElementsByClassName('theme-hope-content');
-  let contentElm = null;
-  if (contentElms.length > 0) {
-    contentElm = contentElms[0];
-  }
-
-  if (!contentElm) {
-    return;
-  }
-
-  const images = contentElm.getElementsByTagName('img');
-  const imaArr = [];
-  for (let i = 0; i < images.length; i++) {
-    const elm = images[i];
-    if (type === 'bind') {
-      imaArr.push({
-        alt: elm.getAttribute('alt'),
-        src: elm.getAttribute('src'),
-      });
-    }
-
-    elm.removeEventListener('click', ImageClick);
-    if (type === 'bind') {
-      elm.addEventListener('click', ImageClick);
-    }
-  }
-  ImageArr.value = imaArr;
-}
-
-let mouseStatus = false;
-let mouseDownX = 0;
-let mouseDownY = 0;
-let imgElmLeft = 0;
-let imgElmTop = 0;
-function Mousemove(e) {
-  if (!mouseStatus) {
-    return;
-  }
-
-  const xDiff = e.clientX - mouseDownX;
-  const yDiff = e.clientY - mouseDownY;
-  const left = imgElmLeft + xDiff;
-  const top = imgElmTop + yDiff;
-
-  // 移动赋值
-  const imgElm = document.getElementById('Mo7PreviewBox-img');
-  imgElm.style.left = left + 'px';
-  imgElm.style.top = top + 'px';
-}
-
-function ImgMouseUp() {
-  const imgElm = document.getElementById('Mo7PreviewBox-img');
-  imgElm.style.cursor = 'default';
-  mouseStatus = false;
-  mouseDownX = 0;
-  mouseDownY = 0;
-  imgElmLeft = 0;
-  imgElmTop = 0;
-}
-
-function ImgMouseDown(e) {
-  const imgElm = document.getElementById('Mo7PreviewBox-img');
-  imgElm.style.cursor = 'move';
-  mouseStatus = true;
-
-  console.log('按下', e);
-
-  mouseDownX = e.clientX;
-  mouseDownY = e.clientY;
-
-  let computedStyle;
-  if (window.getComputedStyle) {
-    computedStyle = window.getComputedStyle(imgElm, null);
-  } else {
-    computedStyle = (imgElm as any).currentStyle; //兼容IE的写法
-  }
-
-  imgElmLeft = parseInt(computedStyle.left);
-  imgElmTop = parseInt(computedStyle.top);
-}
-
-function on_mousemove(e) {
-  if (e.type === 'touchmove') {
-    if (e.touches.length == 1) {
-      Mousemove(e.touches[0]);
-    }
-  }
-  if (e.type === 'mousemove') {
-    Mousemove(e);
-  }
-}
-
-function on_mouseup(e) {
-  ImgMouseUp();
-}
-
-function on_mousedown(e) {
-  if (e.type === 'touchstart') {
-    if (e.touches.length == 1) {
-      ImgMouseDown(e.touches[0]);
-    }
-  }
-  if (e.type === 'mousedown') {
-    ImgMouseDown(e);
-  }
-}
-
-onMounted(() => {
-  nextTick(() => {
-    InitPreviewImage('bind');
-  });
-
-  const router = useRouter();
-  router.beforeEach((to, from) => {
-    if (to.path === from.path) {
-      return;
-    }
-    InitPreviewImage('unbind');
-  });
-
-  router.afterEach((to, from) => {
-    if (to.path === from.path) {
-      return;
-    }
-    nextTick(() => {
-      setTimeout(() => {
-        InitPreviewImage('bind');
-      }, 1000);
-    });
-  });
-});
-</script>
-
 <template>
   <ClientOnly>
     <div
@@ -244,6 +7,7 @@ onMounted(() => {
       @touchmove="on_mousemove"
       @mouseup="on_mouseup"
       @touchend="on_mouseup"
+      @click="closeOnOutsideClick"
     >
       <div class="Mo7PreviewBox-topBar">
         <div class="btn" @click="leftFunc" :class="{ hide: CurrentImgIdx === 0 }">
@@ -252,19 +16,19 @@ onMounted(() => {
         <div class="btn" @click="rightFunc" :class="{ hide: CurrentImgIdx === ImageArr.length - 1 }">
           <MyIcon class="icon" name="xiangyou"></MyIcon>
         </div>
-        <div class="btn" @click.stop="zoomFunc">
+        <div class="btn" @click="zoomFunc">
           <MyIcon class="icon" name="fangda"></MyIcon>
         </div>
-        <div class="btn" @click.stop="shrinkFunc">
+        <div class="btn" @click="shrinkFunc">
           <MyIcon class="icon" name="suoxiao"></MyIcon>
         </div>
-        <div class="btn" @click.stop="fullscreenFunc">
+        <div class="btn" @click="fullscreenFunc">
           <MyIcon class="icon" name="quanping"></MyIcon>
         </div>
         <a class="btn" :href="ImageArr[CurrentImgIdx].src" download>
           <MyIcon class="icon" name="xiazai"></MyIcon>
         </a>
-        <div class="btn" @click.stop="ClosePreviewBox">
+        <div class="btn" @click="ClosePreviewBox">
           <MyIcon class="icon" name="guanbi"></MyIcon>
         </div>
       </div>
@@ -273,22 +37,266 @@ onMounted(() => {
         id="Mo7PreviewBox-img"
         @mousedown="on_mousedown"
         @touchstart="on_mousedown"
-        :src="ImageArr[CurrentImgIdx].src"
-        :alt="ImageArr[CurrentImgIdx].alt"
-        srcset=""
+        @click.stop="() => {}"
+        :src="ImageArr[CurrentImgIdx]?.src"
+        :alt="ImageArr[CurrentImgIdx]?.alt"
       />
 
       <div class="Mo7PreviewBox-idxView">
         <div class="Mo7PreviewBox-idxView-box">
           <div class="Mo7PreviewBox-idxView-idx">{{ CurrentImgIdx + 1 }}/{{ ImageArr.length }}</div>
-          <div class="Mo7PreviewBox-idxView-alt" v-if="ImageArr[CurrentImgIdx].alt">
-            {{ ImageArr[CurrentImgIdx].alt }}
+          <div class="Mo7PreviewBox-idxView-alt" v-if="ImageArr[CurrentImgIdx]?.alt">
+            {{ ImageArr[CurrentImgIdx]?.alt }}
           </div>
         </div>
       </div>
     </div>
   </ClientOnly>
 </template>
+
+<script setup lang="ts">
+import { onMounted, nextTick, ref } from 'vue';
+import { useRouter } from 'vue-router';
+import MyIcon from './MyIcon.vue';
+
+let ImageArr = ref<{ src: string; alt: string }[]>([]);
+let ShowPreviewBox = ref(false);
+let CurrentImgIdx = ref(0);
+
+// 关闭预览框
+function ClosePreviewBox() {
+  ShowPreviewBox.value = false;
+  document.body.style.overflow = 'auto';
+}
+
+// 点击非图片区域关闭预览框
+function closeOnOutsideClick(e: MouseEvent) {
+  const imgElm = document.getElementById('Mo7PreviewBox-img') as HTMLImageElement | null;
+  if (!imgElm) return;
+
+  const rect = imgElm.getBoundingClientRect();
+  const isClickInsideImg =
+    e.clientX >= rect.left && e.clientX <= rect.right && e.clientY >= rect.top && e.clientY <= rect.bottom;
+
+  if (!isClickInsideImg) {
+    ClosePreviewBox();
+  }
+}
+
+// 图片放大
+function zoomFunc() {
+  const imgElm = document.getElementById('Mo7PreviewBox-img') as HTMLImageElement | null;
+  if (imgElm) {
+    let width = imgElm.clientWidth;
+    let height = imgElm.clientHeight;
+    width *= 1.1;
+    height *= 1.1;
+    imgElm.style.width = `${width}px`;
+    imgElm.style.height = `${height}px`;
+  }
+}
+
+// 图片缩小
+function shrinkFunc() {
+  const imgElm = document.getElementById('Mo7PreviewBox-img') as HTMLImageElement | null;
+  if (imgElm) {
+    let width = imgElm.clientWidth;
+    let height = imgElm.clientHeight;
+    width *= 0.9;
+    height *= 0.9;
+    imgElm.style.width = `${width}px`;
+    imgElm.style.height = `${height}px`;
+  }
+}
+
+// 全屏查看
+function fullscreenFunc() {
+  const imgElm = document.getElementById('Mo7PreviewBox-img') as HTMLImageElement | null;
+  if (imgElm) {
+    window.open(imgElm.src);
+  }
+}
+
+// 左滑动
+function leftFunc() {
+  if (CurrentImgIdx.value > 0) {
+    CurrentImgIdx.value--;
+  }
+  nextTick(() => {
+    ImageBoxReset();
+  });
+}
+
+// 右滑动
+function rightFunc() {
+  if (CurrentImgIdx.value < ImageArr.value.length - 1) {
+    CurrentImgIdx.value++;
+  }
+  nextTick(() => {
+    ImageBoxReset();
+  });
+}
+
+// 重置图片大小
+function ImageBoxReset() {
+  const boxElm = document.getElementById('Mo7PreviewBox') as HTMLElement | null;
+  const imgElm = document.getElementById('Mo7PreviewBox-img') as HTMLImageElement | null;
+  if (!boxElm || !imgElm) return;
+
+  const boxWidth = boxElm.clientWidth;
+  const boxHeight = boxElm.clientHeight;
+
+  imgElm.style.width = `${boxWidth * 0.9}px`;
+  imgElm.style.height = '';
+  imgElm.style.maxWidth = '';
+  imgElm.style.maxHeight = '';
+  imgElm.style.left = '';
+  imgElm.style.top = '';
+  imgElm.style.cursor = '';
+
+  const height = imgElm.clientHeight;
+  if (height > boxHeight) {
+    imgElm.style.width = '';
+    imgElm.style.height = `${boxHeight * 0.9}px`;
+  }
+}
+
+// 图片点击事件
+function ImageClick(e: MouseEvent) {
+  const elm = e.target as HTMLImageElement;
+  const src = elm.getAttribute('src');
+  if (!src) return;
+
+  const index = ImageArr.value.findIndex((img) => img.src === src);
+  if (index !== -1) {
+    CurrentImgIdx.value = index;
+    ShowPreviewBox.value = true;
+    document.body.style.overflow = 'hidden';
+    nextTick(() => {
+      ImageBoxReset();
+    });
+  }
+}
+
+// 初始化图片预览
+function InitPreviewImage(type: 'bind' | 'unbind') {
+  const contentElms = document.getElementsByClassName('theme-hope-content');
+  const contentElm = contentElms.length > 0 ? contentElms[0] : null;
+
+  if (!contentElm) return;
+
+  const images = contentElm.getElementsByTagName('img');
+  const imaArr: { src: string; alt: string }[] = [];
+
+  for (const elm of images) {
+    if (type === 'bind') {
+      imaArr.push({
+        alt: elm.getAttribute('alt') || '',
+        src: elm.getAttribute('src') || '',
+      });
+    }
+
+    elm.removeEventListener('click', ImageClick);
+    if (type === 'bind') {
+      elm.addEventListener('click', ImageClick);
+    }
+  }
+
+  ImageArr.value = imaArr;
+}
+
+// 鼠标拖动图片
+let mouseStatus = false;
+let mouseDownX = 0;
+let mouseDownY = 0;
+let imgElmLeft = 0;
+let imgElmTop = 0;
+
+function Mousemove(e: MouseEvent | TouchEvent) {
+  if (!mouseStatus) return;
+
+  const xDiff = e instanceof MouseEvent ? e.clientX - mouseDownX : e.touches[0].clientX - mouseDownX;
+  const yDiff = e instanceof MouseEvent ? e.clientY - mouseDownY : e.touches[0].clientY - mouseDownY;
+  const left = imgElmLeft + xDiff;
+  const top = imgElmTop + yDiff;
+
+  const imgElm = document.getElementById('Mo7PreviewBox-img') as HTMLImageElement | null;
+  if (imgElm) {
+    imgElm.style.left = `${left}px`;
+    imgElm.style.top = `${top}px`;
+  }
+}
+
+function ImgMouseUp() {
+  const imgElm = document.getElementById('Mo7PreviewBox-img') as HTMLImageElement | null;
+  if (imgElm) {
+    imgElm.style.cursor = 'default';
+  }
+
+  mouseStatus = false;
+  mouseDownX = 0;
+  mouseDownY = 0;
+  imgElmLeft = 0;
+  imgElmTop = 0;
+}
+
+function ImgMouseDown(e: MouseEvent | TouchEvent) {
+  const imgElm = document.getElementById('Mo7PreviewBox-img') as HTMLImageElement | null;
+  if (!imgElm) return;
+
+  imgElm.style.cursor = 'move';
+  mouseStatus = true;
+
+  if (e instanceof MouseEvent) {
+    mouseDownX = e.clientX;
+    mouseDownY = e.clientY;
+  } else {
+    mouseDownX = e.touches[0].clientX;
+    mouseDownY = e.touches[0].clientY;
+  }
+
+  const computedStyle = window.getComputedStyle(imgElm);
+  imgElmLeft = parseInt(computedStyle.left, 10) || 0;
+  imgElmTop = parseInt(computedStyle.top, 10) || 0;
+}
+
+// 监听鼠标事件
+function on_mousemove(e: MouseEvent | TouchEvent) {
+  Mousemove(e);
+}
+
+function on_mouseup(e: MouseEvent | TouchEvent) {
+  ImgMouseUp();
+}
+
+function on_mousedown(e: MouseEvent | TouchEvent) {
+  ImgMouseDown(e);
+}
+
+// 初始化
+onMounted(() => {
+  nextTick(() => {
+    InitPreviewImage('bind');
+  });
+
+  const router = useRouter();
+  router.beforeEach((to, from) => {
+    if (to.path !== from.path) {
+      InitPreviewImage('unbind');
+    }
+  });
+
+  router.afterEach((to, from) => {
+    if (to.path !== from.path) {
+      nextTick(() => {
+        setTimeout(() => {
+          InitPreviewImage('bind');
+        }, 1000);
+      });
+    }
+  });
+});
+</script>
 
 <style lang="scss">
 .theme-hope-content {
