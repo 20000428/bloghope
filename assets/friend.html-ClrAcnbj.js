bf342214536269353db10e775e3a630bc1f3fa7f
xuuub66 <504077806@qq.com>
2025-04-27 14:27:10 +0800

    deploy

---
+++ assets/friend.html-ClrAcnbj.js
@@ -0,0 +1,82 @@
+import{_ as d}from"./plugin-vue_export-helper-DlAUqK2U.js";import{r as h,c as p,a as t,d as r,w as n,b as i,f as c,o as k,e as s}from"./app-BDUdU_nv.js";const g={},m=i("h1",{id:"友链",tabindex:"-1"},[i("a",{class:"header-anchor",href:"#友链"},[i("span",null,"友链")])],-1),u=c(`<hr><div class="hint-container tip"><p class="hint-container-title">欢迎交换友情链接</p><p>在评论区按照如下格式留言:</p><div class="language-yaml line-numbers-mode" data-highlighter="shiki" data-ext="yaml" data-title="yaml" style="--shiki-light:#383A42;--shiki-dark:#abb2bf;--shiki-light-bg:#FAFAFA;--shiki-dark-bg:#282c34;"><pre class="shiki shiki-themes one-light one-dark-pro vp-code"><code><span class="line"><span style="--shiki-light:#E45649;--shiki-dark:#E06C75;">昵称</span><span style="--shiki-light:#383A42;--shiki-dark:#ABB2BF;">: </span><span style="--shiki-light:#50A14F;--shiki-dark:#98C379;">昵称</span></span>
+<span class="line"><span style="--shiki-light:#E45649;--shiki-dark:#E06C75;">描述</span><span style="--shiki-light:#383A42;--shiki-dark:#ABB2BF;">: </span><span style="--shiki-light:#50A14F;--shiki-dark:#98C379;">简介</span></span>
+<span class="line"><span style="--shiki-light:#E45649;--shiki-dark:#E06C75;">地址</span><span style="--shiki-light:#383A42;--shiki-dark:#ABB2BF;">: </span><span style="--shiki-light:#50A14F;--shiki-dark:#98C379;">主页地址</span></span>
+<span class="line"><span style="--shiki-light:#E45649;--shiki-dark:#E06C75;">头像</span><span style="--shiki-light:#383A42;--shiki-dark:#ABB2BF;">: </span><span style="--shiki-light:#50A14F;--shiki-dark:#98C379;">头像链接</span></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>然后将本站的信息添加到你的站点中</p></div><h3 id="本站信息如下" tabindex="-1"><a class="header-anchor" href="#本站信息如下"><span>本站信息如下:</span></a></h3>`,3),b=i("div",{class:"language-yaml line-numbers-mode","data-highlighter":"shiki","data-ext":"yaml","data-title":"yaml",style:{"--shiki-light":"#383A42","--shiki-dark":"#abb2bf","--shiki-light-bg":"#FAFAFA","--shiki-dark-bg":"#282c34"}},[i("pre",{class:"shiki shiki-themes one-light one-dark-pro vp-code"},[i("code",null,[i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#E45649","--shiki-dark":"#E06C75"}},"昵称"),i("span",{style:{"--shiki-light":"#383A42","--shiki-dark":"#ABB2BF"}},": "),i("span",{style:{"--shiki-light":"#50A14F","--shiki-dark":"#98C379"}},"叶随风")]),s(`
+`),i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#E45649","--shiki-dark":"#E06C75"}},"描述"),i("span",{style:{"--shiki-light":"#383A42","--shiki-dark":"#ABB2BF"}},": "),i("span",{style:{"--shiki-light":"#50A14F","--shiki-dark":"#98C379"}},"世界和平,身体健康。")]),s(`
+`),i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#E45649","--shiki-dark":"#E06C75"}},"地址"),i("span",{style:{"--shiki-light":"#383A42","--shiki-dark":"#ABB2BF"}},": "),i("span",{style:{"--shiki-light":"#50A14F","--shiki-dark":"#98C379"}},"https://20000428.github.io/bloghope/")]),s(`
+`),i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#E45649","--shiki-dark":"#E06C75"}},"头像"),i("span",{style:{"--shiki-light":"#383A42","--shiki-dark":"#ABB2BF"}},": "),i("span",{style:{"--shiki-light":"#50A14F","--shiki-dark":"#98C379"}},"https://cdn.jsdelivr.net/gh/20000428/PictureBed/img/huge.png")])])]),i("div",{class:"line-numbers","aria-hidden":"true",style:{"counter-reset":"line-number 0"}},[i("div",{class:"line-number"}),i("div",{class:"line-number"}),i("div",{class:"line-number"}),i("div",{class:"line-number"})])],-1),y=i("div",{class:"language-json line-numbers-mode","data-highlighter":"shiki","data-ext":"json","data-title":"json",style:{"--shiki-light":"#383A42","--shiki-dark":"#abb2bf","--shiki-light-bg":"#FAFAFA","--shiki-dark-bg":"#282c34"}},[i("pre",{class:"shiki shiki-themes one-light one-dark-pro vp-code"},[i("code",null,[i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#383A42","--shiki-dark":"#ABB2BF"}},"{")]),s(`
+`),i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#E45649","--shiki-dark":"#E06C75"}},'  "name"'),i("span",{style:{"--shiki-light":"#383A42","--shiki-dark":"#ABB2BF"}},": "),i("span",{style:{"--shiki-light":"#50A14F","--shiki-dark":"#98C379"}},'"叶随风"'),i("span",{style:{"--shiki-light":"#383A42","--shiki-dark":"#ABB2BF"}},",")]),s(`
+`),i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#E45649","--shiki-dark":"#E06C75"}},'  "description"'),i("span",{style:{"--shiki-light":"#383A42","--shiki-dark":"#ABB2BF"}},": "),i("span",{style:{"--shiki-light":"#50A14F","--shiki-dark":"#98C379"}},'"世界和平,身体健康。"'),i("span",{style:{"--shiki-light":"#383A42","--shiki-dark":"#ABB2BF"}},",")]),s(`
+`),i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#E45649","--shiki-dark":"#E06C75"}},'  "link"'),i("span",{style:{"--shiki-light":"#383A42","--shiki-dark":"#ABB2BF"}},": "),i("span",{style:{"--shiki-light":"#50A14F","--shiki-dark":"#98C379"}},'"https://20000428.github.io/bloghope/"'),i("span",{style:{"--shiki-light":"#383A42","--shiki-dark":"#ABB2BF"}},",")]),s(`
+`),i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#E45649","--shiki-dark":"#E06C75"}},'  "avatar"'),i("span",{style:{"--shiki-light":"#383A42","--shiki-dark":"#ABB2BF"}},": "),i("span",{style:{"--shiki-light":"#50A14F","--shiki-dark":"#98C379"}},'"https://cdn.jsdelivr.net/gh/20000428/PictureBed/img/huge.png"')]),s(`
+`),i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#383A42","--shiki-dark":"#ABB2BF"}},"}")])])]),i("div",{class:"line-numbers","aria-hidden":"true",style:{"counter-reset":"line-number 0"}},[i("div",{class:"line-number"}),i("div",{class:"line-number"}),i("div",{class:"line-number"}),i("div",{class:"line-number"}),i("div",{class:"line-number"}),i("div",{class:"line-number"})])],-1);function A(v,B){const e=h("VPCard"),o=h("CodeTabs");return k(),p("div",null,[m,t(e,{title:"墨七",desc:"专心致志做事，大气温和待人。",logo:"https://file.mo7.cc/static/lxh_gif/lxh_71.gif",link:"https://mo7.cc/",background:"rgba(253, 230, 138, 0.15)"}),t(e,{title:"夜猫君",desc:"欲把西湖比西子，淡妆浓抹总相宜。",logo:"https://cdn.jsdelivr.net/gh/20000428/PictureBed//img/yemaojun.jpg",link:"https://www.yemaojun.top/",background:"rgba(253, 230, 138, 0.15)"}),t(e,{title:"Seele_1018",desc:"我很懒，所以这里没有简介！",logo:"https://cdn.jsdelivr.net/gh/20000428/PictureBed//img/hong.jpg",link:"https://blog.csdn.net/qq_44826887?type=blog",background:"rgba(253, 230, 138, 0.15)"}),r(` <VPCard
+  title="菜园前端"
+  desc="前端学习笔记分享、小白都能看懂的笔记"
+  logo="https://note.noxussj.top/logo.png"
+  link="https://note.noxussj.top/"
+  background="rgba(253, 230, 138, 0.15)"
+/>
+
+<VPCard
+  title="粥里有勺糖"
+  desc="你的指尖,拥有改变世界的力量😄"
+  logo="https://sugarat.top/favicon.ico"
+  link="https://sugarat.top/aboutme.html"
+  background="rgba(253, 230, 138, 0.15)"
+/>
+
+
+<VPCard
+  title="梦爱吃鱼"
+  desc="不负心灵,不负今生"
+  logo="https://blog.bsgun.cn/"
+  link="https://blog.bsgun.cn/"
+  background="rgba(253, 230, 138, 0.15)"
+/> `),t(e,{title:"Mr.Hope",desc:"Where there is light, there is hope",logo:"https://mister-hope.com/logo.svg",link:"https://mister-hope.com/",background:"rgba(253, 230, 138, 0.15)"}),r(` <VPCard
+  title="Bing🐣"
+  desc="鱼跃此时海 花开彼岸天。"
+  logo="https://liubing.me/logo.svg"
+  link="https://liubing.me"
+/>
+
+<VPCard
+  title="AI悦创"
+  desc="浅者见浅，深者见深——黄家宝。"
+  logo="https://bornforthis.cn/aiyc.svg"
+  link="https://bornforthis.cn"
+/>
+
+<VPCard
+  title="叉叉白"
+  desc="菜鸡程序员"
+  logo="https://blog.xxwhite.com/assets/img/avatar.jpg"
+  link="https://blog.xxwhite.com"
+/>
+
+<VPCard
+  title="测试猿全栈知识体系"
+  desc="质量是1到100的事情！"
+  logo="https://testyuan1024.com/avatar.png"
+  link="https://testyuan1024.com"
+/>
+
+<VPCard
+  title="CoderChen"
+  desc="这里是CK的小世界!"
+  logo="https://imgtable.oss-cn-chengdu.aliyuncs.com/img/ECC5A66FD27C9118F1B57C4451B3B045.jpg"
+  link="http://ckblogs.cn"
+/>
+
+<VPCard
+  title="Salvely"
+  desc="Salvely"
+  logo="https://salvely.github.io/logo.svg"
+  link="https://salvely.github.io"
+/>
+
+<VPCard
+  title="JavaQuan"
+  desc="一只爱折腾的攻城狮"
+  logo="https://javaquan.cn/img/logo.png"
+  link="https://javaquan.cn"
+/>  `),u,t(o,{id:"22",data:[{id:"label"},{id:"json"}],"tab-id":"shell"},{title0:n(({value:a,isActive:l})=>[s("label")]),title1:n(({value:a,isActive:l})=>[s("json")]),tab0:n(({value:a,isActive:l})=>[b]),tab1:n(({value:a,isActive:l})=>[y]),_:1})])}const F=d(g,[["render",A],["__file","friend.html.vue"]]),_=JSON.parse('{"path":"/favorite/links/friend.html","title":"友链","lang":"zh-CN","frontmatter":{"icon":"youlian","order":1,"pageInfo":["Author","PageView"],"description":"友链","head":[["meta",{"property":"og:url","content":"https://blog.ysf.cc/favorite/links/friend.html"}],["meta",{"property":"og:site_name","content":"LeafinWind"}],["meta",{"property":"og:title","content":"友链"}],["meta",{"property":"og:description","content":"友链"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2025-01-22T05:30:50.000Z"}],["meta",{"property":"article:author","content":"叶随风"}],["meta",{"property":"article:modified_time","content":"2025-01-22T05:30:50.000Z"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"友链\\",\\"image\\":[\\"\\"],\\"dateModified\\":\\"2025-01-22T05:30:50.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"叶随风\\",\\"email\\":\\"504077806@qq.com\\"}]}"],["link",{"rel":"alternate","type":"application/atom+xml","href":"https://blog.ysf.cc/atom.xml","title":"LeafinWind Atom Feed"}],["link",{"rel":"alternate","type":"application/json","href":"https://blog.ysf.cc/feed.json","title":"LeafinWind JSON Feed"}],["link",{"rel":"alternate","type":"application/rss+xml","href":"https://blog.ysf.cc/rss.xml","title":"LeafinWind RSS Feed"}]]},"headers":[{"level":3,"title":"本站信息如下:","slug":"本站信息如下","link":"#本站信息如下","children":[]}],"git":{"createdTime":1721897258000,"updatedTime":1737523850000,"contributors":[{"name":"xuuub66","email":"504077806@qq.com","commits":10}]},"readingTime":{"minutes":1.71,"words":513},"filePathRelative":"favorite/links/friend.md","localizedDate":"2024年7月25日","excerpt":"\\n","autoDesc":true}');export{F as comp,_ as data};
