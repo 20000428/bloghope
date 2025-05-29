5eddd30f8d4ce9ccf8d443c94f2d69a47c867cf8
xuuub66 <504077806@qq.com>
2025-05-29 17:25:57 +0800

    Init

--- _shell/deploy01.sh
+++ _shell/deploy01.sh
@@ -10,5 +10,4 @@ git remote rm origin
 git remote add origin https://github.com/20000428/bloghope.git
 #git push -u origin "master"
 #之后更改可以用，强制推送到远程master分支
-# git push origin master --force
-git push -f git@github.com:20000428/bloghope.git master
+git push origin master --force
